a140fa3c58249f9a6013bf6ed895abc77c3ab962
thisisaaronland <devnull@localhost>
2018-08-14 10:35:42 -0700

    belongs-to as JSON and CSV

--- cmd/wof-belongs-to.go
+++ cmd/wof-belongs-to.go
@@ -1,13 +1,134 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"github.com/whosonfirst/go-whosonfirst-cli/flags"
 	"github.com/whosonfirst/go-whosonfirst-geojson-v2"
-	"github.com/whosonfirst/go-whosonfirst-travel"
+	"github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/whosonfirst"
 	"github.com/whosonfirst/go-whosonfirst-travel/traveler"
+	"io"
 	"log"
+	"os"
+	"strconv"
+	"sync"
 )
+
+// please move all the BelongsToResult and BelongsToResultSet stuff
+// in to a proper re-usalbe package (20180814/thisisaaronland)
+
+type BelongsToResultSet struct {
+	results []*BelongsToResult
+	mu      *sync.RWMutex
+}
+
+func NewBelongsToResultSet() (*BelongsToResultSet, error) {
+
+	results := make([]*BelongsToResult, 0)
+	mu := new(sync.RWMutex)
+
+	rs := BelongsToResultSet{
+		results: results,
+		mu:      mu,
+	}
+
+	return &rs, nil
+}
+
+func (rs *BelongsToResultSet) AddResult(r *BelongsToResult) error {
+
+	rs.mu.Lock()
+	defer rs.mu.Unlock()
+
+	rs.results = append(rs.results, r)
+	return nil
+}
+
+func (rs *BelongsToResultSet) Results() []*BelongsToResult {
+
+	return rs.results
+}
+
+func (rs *BelongsToResultSet) AsJSON(wr io.Writer) error {
+
+	b, err := json.Marshal(rs.Results())
+
+	if err != nil {
+		return err
+	}
+
+	br := bytes.NewReader(b)
+	_, err = io.Copy(wr, br)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (rs *BelongsToResultSet) AsCSV(wr io.Writer) error {
+
+	csv_wr := csv.NewWriter(wr)
+
+	for i, r := range rs.Results() {
+
+		if i == 0 {
+
+			head, _ := r.ToCSVHeader()
+			err := csv_wr.Write(head)
+
+			if err != nil {
+				return err
+			}
+
+		}
+
+		out, _ := r.ToCSVRow()
+		err := csv_wr.Write(out)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	csv_wr.Flush()
+
+	return nil
+}
+
+type BelongsToResult struct {
+	BelongsToId int64  `json:"belongs_to"`
+	Id          int64  `json:"id"`
+	Placetype   string `json:"placetype"`
+	Label       string `json:"label"`
+}
+
+func (r *BelongsToResult) ToCSVHeader() ([]string, error) {
+
+	head := []string{
+		"belongs_to",
+		"id",
+		"placetype",
+		"label",
+	}
+
+	return head, nil
+}
+
+func (r *BelongsToResult) ToCSVRow() ([]string, error) {
+
+	out := []string{
+		strconv.FormatInt(r.BelongsToId, 10),
+		strconv.FormatInt(r.Id, 10),
+		r.Placetype,
+		r.Label,
+	}
+
+	return out, nil
+}
 
 func main() {
 
@@ -21,19 +142,25 @@ func main() {
 	flag.Var(&exclude_placetype, "exclude-placetype", "...")
 
 	mode := flag.String("mode", "repo", "...")
+	as_json := flag.Bool("json", false, "...")
 
 	flag.Parse()
 
-	default_cb, err := travel.DefaultTravelFunc()
+	rs, err := NewBelongsToResultSet()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	cb := func(r *BelongsToResult) error {
+
+		return rs.AddResult(r)
+	}
+
 	// we should make this a canned TravelFunc once we figure out
 	// what the method signature looks like... (20180314/thisisaaronland)
 
-	cb := func(f geojson.Feature, step int64) error {
+	filter_cb := func(f geojson.Feature, belongsto_id int64) error {
 
 		pt := f.Placetype()
 
@@ -51,18 +178,43 @@ func main() {
 			}
 		}
 
-		return default_cb(f, step)
+		r := BelongsToResult{
+			BelongsToId: belongsto_id,
+			Id:          whosonfirst.Id(f),
+			Placetype:   pt,
+			Label:       whosonfirst.LabelOrDerived(f),
+		}
+
+		return cb(&r)
 	}
 
 	t, err := traveler.NewDefaultBelongsToTraveler()
 	t.Mode = *mode
 	t.BelongsTo = belongs_to
-	t.Callback = cb
+	t.Callback = filter_cb
 
 	paths := flag.Args()
 	err = t.Travel(paths...)
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *as_json {
+
+		err := rs.AsJSON(os.Stdout)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+
+		err := rs.AsCSV(os.Stdout)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	}
 }
